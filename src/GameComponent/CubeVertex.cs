ec199e594f98f1d390cb063e5877450c092aa23a
TLBail <62939298+TLBail@users.noreply.github.com>
2022-10-18 09:29:33 +0200

    backing up for big vertex changes

--- src/GameComponent/CubeVertex.cs
+++ src/GameComponent/CubeVertex.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Silk.NET.Maths;
+﻿using Silk.NET.Maths;
 
 namespace MinecraftCloneSilk.src.GameComponent
 {
     public struct CubeVertex
     {
-        Vector3D<float> position;
-        Vector2D<float> texCoords;
+        public Vector3D<float> position;
+        public Vector2D<float> texCoords;
 
         public CubeVertex(Vector3D<float> position, Vector2D<float> texCoords)
         {
