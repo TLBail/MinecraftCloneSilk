7e67e769bcd29a6265c4b46ddd1b97f9708fd5d0
TLBail <62939298+TLBail@users.noreply.github.com>
2022-10-29 17:01:56 +0200

    factory block less expensive block data

---
+++ src/GameComponent/BlockData.cs
@@ -0,0 +1,12 @@
+﻿namespace MinecraftCloneSilk.GameComponent;
+
+public struct BlockData
+{
+    public string name; //block id
+
+    public BlockData(string name)
+    {
+        this.name = name;
+    }
+}
+
