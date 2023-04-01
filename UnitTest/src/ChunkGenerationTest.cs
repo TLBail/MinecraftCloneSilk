705524c18bdfb0ee8b8e8e5872b6f33731e55d03
TLBail <62939298+TLBail@users.noreply.github.com>
2023-04-01 14:24:33 +0200

    update test

--- UnitTest/src/ChunkGenerationTest.cs
+++ UnitTest/src/ChunkGenerationTest.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Running;
 using MinecraftCloneSilk.Core;
 using MinecraftCloneSilk.GameComponent;
 using MinecraftCloneSilk.Model;
+using MinecraftCloneSilk.Model.NChunk;
 using MinecraftCloneSilk.UI;
 using NUnit.Framework.Interfaces;
 using Silk.NET.Maths;
@@ -18,6 +19,7 @@ public class ChunkGenerationTest
 
     [OneTimeSetUp]
     public  void initGame() {
+        
         Scene scene = new Scene(new List<InitGameData>()
             {
                 new (typeof(Player).FullName),
@@ -31,28 +33,181 @@ public class ChunkGenerationTest
         });
         gameThread.Start();
     }
+    [SetUp]
+    public async Task setup() {
+        await game.waitForFrame(1);
+    }
 
+    [TearDown]
+    public async Task tearDown() {
+        await game.waitForFrame(10);
+        world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.clear();
+        await game.waitForFrame(10);
+    }
+    
     [OneTimeTearDown]
     public void endGame() {
         game.Stop();
         gameThread.Join();
+        
+        DirectoryInfo directory = Directory.CreateDirectory("./Worlds/newWorld");
+        foreach (var file in         Directory.GetFiles("./Worlds/newWorld")) {
+            File.Delete(file);
+        }
     }
     
+    
+
     [Test]
-    public async Task testOneChunkGeneration() {
+    public async Task TestOneChunkGeneration() {
          await game.waitForFrame(1);
          world = (World)game.gameObjects[typeof(World).FullName];
+         world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){Vector3D<int>.Zero});
+         await game.waitForFrame(3);
          Assert.NotNull( world.getBlock(Vector3D<int>.Zero));
     }
     
     [Test]
-    public async Task testDeletingBlock() {
+    public async Task TestDeletingBlock() {
         await game.waitForFrame(1);
         world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){Vector3D<int>.Zero});
+        await game.waitForFrame(3);
         world.setBlock(BlockFactory.AIR_BLOCK, Vector3D<int>.Zero);
         var block = world.getBlock(Vector3D<int>.Zero);
-        Assert.True(block.Equals(BlockFactory.AIR_BLOCK));
+        Assert.True(block.name.Equals(BlockFactory.AIR_BLOCK));
     }
 
+
+    [Test]
+    public async Task TestUnloading() {
+        await game.waitForFrame(1);
+        world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){Vector3D<int>.Zero});
+        await game.waitForFrame(3);
+        world.setBlock(BlockFactory.AIR_BLOCK, Vector3D<int>.Zero);
+        var block = world.getBlock(Vector3D<int>.Zero);
+        
+        world.chunkManager.tryToUnloadChunk(Vector3D<int>.Zero);
+        await game.waitForFrame(1);
+        Assert.False(world.chunkManager.getChunksList().Any((chunk) => chunk.position.Equals(Vector3D<int>.Zero)));
+    }
     
+    
+    [Test]
+    public async Task TestChunkHaveBeenSave() {
+        await game.waitForFrame(1);
+        world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){Vector3D<int>.Zero});
+        await game.waitForFrame(3);
+        world.setBlock(BlockFactory.AIR_BLOCK, Vector3D<int>.Zero);
+        var block = world.getBlock(Vector3D<int>.Zero);
+        
+        world.chunkManager.tryToUnloadChunk(Vector3D<int>.Zero);
+        await game.waitForFrame(10);
+        
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){Vector3D<int>.Zero});
+        await game.waitForFrame(3);
+        Assert.True(world.getBlock(Vector3D<int>.Zero).name.Equals(BlockFactory.AIR_BLOCK));
+    }
+
+
+    [Test]
+    public async Task TestChunkSaveWhenblockGenerated() {
+        await game.waitForFrame(1);
+        world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>()
+        {
+            Vector3D<int>.Zero,
+            new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0),
+        });
+        await game.waitForFrame(10);
+        world.setBlock(BlockFactory.AIR_BLOCK, new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0));
+        
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0));
+        await game.waitForFrame(10);
+        
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0)});
+        await game.waitForFrame(10);
+        Assert.True(world.getBlock(new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0)).name.Equals(BlockFactory.AIR_BLOCK));
+
+    }
+    
+    
+    [Test]
+    public async Task TestChunkSaveWhenEmpty() {
+        await game.waitForFrame(1);
+        world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>()
+        {
+            Vector3D<int>.Zero,
+            new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0),
+            new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0),
+
+        });
+        await game.waitForFrame(10);
+        world.setBlock("metal", new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0));
+
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0));
+        await game.waitForFrame(10);
+        Assert.True(world.chunkManager.getChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0)).chunkState == ChunkState.BLOCKGENERATED);
+        
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0));
+        await game.waitForFrame(10);
+        Assert.True(world.chunkManager.getChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0)).chunkState == ChunkState.BLOCKGENERATED);
+
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0));
+        await game.waitForFrame(10);
+
+        Assert.False(world.chunkManager.getChunksList().Any((chunk) => chunk.position.Equals(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0))));
+
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0)});
+        await game.waitForFrame(10);
+        Assert.True(world.getBlock(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0)).name.Equals("metal"));
+
+    }
+    
+    [Test]
+    public async Task TestVeryFareChunkSaveWhenEmpty() {
+        await game.waitForFrame(1);
+        world = (World)game.gameObjects[typeof(World).FullName];
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>()
+        {
+            Vector3D<int>.Zero,
+            new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0),
+            new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0),
+            new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0),
+        });
+        await game.waitForFrame(10);
+        world.setBlock("metal", new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0));
+
+        
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0));
+        await game.waitForFrame(10);
+        Assert.True(world.chunkManager.getChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0)).chunkState == ChunkState.BLOCKGENERATED);
+        
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0));
+        await game.waitForFrame(10);
+        Assert.True(world.chunkManager.getChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0)).chunkState == ChunkState.BLOCKGENERATED);
+        
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0));
+        await game.waitForFrame(10);
+
+        Assert.False(world.chunkManager.getChunksList().Any((chunk) => chunk.position.Equals(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0))));
+        
+        
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0));
+        await game.waitForFrame(10);
+        Assert.True(world.chunkManager.getChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE, 0, 0)).chunkState == ChunkState.BLOCKGENERATED);
+
+        world.chunkManager.tryToUnloadChunk(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0));
+        await game.waitForFrame(10);
+        Assert.False(world.chunkManager.getChunksList().Any((chunk) => chunk.position.Equals(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 2, 0, 0))));
+
+        world.chunkManager.addChunksToLoad(new List<Vector3D<int>>(){new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0)});
+        await game.waitForFrame(10);
+        Assert.True(world.getBlock(new Vector3D<int>((int)Chunk.CHUNK_SIZE * 3, 0, 0)).name.Equals("metal"));
+
+    }
 }
