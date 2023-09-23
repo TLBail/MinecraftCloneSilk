405f8b3342e71fd4fb571f55e8ae7395b7784d9b
TLBail <62939298+TLBail@users.noreply.github.com>
2023-09-23 18:13:41 +0200

    doc

--- App/src/Core/VertexArrayObject.cs
+++ App/src/Core/VertexArrayObject.cs
@@ -24,11 +24,31 @@ namespace MinecraftCloneSilk.Core
             ebo?.Bind(BufferTargetARB.ElementArrayBuffer);
         }
 
+        /**
+         * <summary>Bind a vertex attribute to a field of the vertex type
+         *  the type of the field is converted to a float vector
+         * </summary>
+         * <param name="index">The index of the vertex attribute</param>
+         * <param name="count">The number of components of the vertex attribute</param>
+         * <param name="type">The type of the vertex attribute</param>
+         * <param name="fieldName">The name of the field of the vertex type</param>
+         */
         public unsafe void VertexAttributePointer(uint index, int count, VertexAttribPointerType type, string fieldName)
         {
             gl.VertexAttribPointer(index, count, type, false,(uint) sizeof(TVertexType), (void*) (Marshal.OffsetOf(typeof(TVertexType), fieldName)));
             gl.EnableVertexAttribArray(index);
         }
+        
+        
+        /**
+         * <summary>Bind a vertex attribute to a field of the vertex type
+         * only the integer types are accepted
+         * </summary>
+         * <param name="index">The index of the vertex attribute</param>
+         * <param name="count">The number of components of the vertex attribute</param>
+         * <param name="type">The type of the vertex attribute</param>
+         * <param name="fieldName">The name of the field of the vertex type</param>
+         */
         public unsafe void VertexAttributeIPointer(uint index, int count,  VertexAttribIType type, string fieldName)
         {
             gl.VertexAttribIPointer(index, count, type,(uint) sizeof(TVertexType), (void*) (Marshal.OffsetOf(typeof(TVertexType), fieldName)));
